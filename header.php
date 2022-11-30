7a7a8a316a97dca9c1ff1850dfb2533d189348fc
sixties03 <64419983+sixties03@users.noreply.github.com>
2022-11-30 06:42:43 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="Tradewinds Employment agency login page">
+		<meta name=viewport content ="width=device-width, initial-scale=1">
+	<title>Tradewinds  Employment agency<title>
+		<title>Attributes selector</title>
+
+</head>
+<body>
+
+	<header>
+		<nav>
+			<a herf="#">
+
+				<img src="img/th-1955237860.jpg" alt="logo">
+			</a>
+			<ul>
+
+
+			<li><a href="index.php">Home</a></li>
+			<style>
+					[class] {
+							text-align:center;
+							Color:#F55415;
+					}
+					.gfg {
+							font-size:120px;
+							font-weight:bold;
+							margin-bottom:-20px;
+					}
+			</style>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">login</a></li>
+			<li><a href="#">About Us</a></li>
+			</ul>
+			<div>
+				<form action="icludes/login.inc.php" method="post">
+					<input type="text" name="mailuid" placeholder="Username/E-mail">
+					<input type="password" name="pwd" placeholder="password">
+					<button type="submit" name="login-submit">login</button>
+				</form>
+				<a href="Signup"></a>
+				<form action="includes/logout" method="post">
+					<button type="submit" name="logout-submit">logout</button>
+				</form>
+			</nav>
+	</header>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+	<main>
+		<div class="wrapper-main">
+			<section class="section-default">
+		<p>You are logged out!</p>
+		<p>You are logged out!</p>
+		</section>
+	</div>
+</main>
+
+<?php
+	require "footer.php";
+?>
